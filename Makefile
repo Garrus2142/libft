79f3efb0532c54320e7c9f6b9f5359a4222c6245
Timothee HUGO <thugo@e2r10p22.42.fr>
2016-11-10 21:41:16 +0100

    Add functions

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: thugo <marvin@42.fr>                       +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2016/11/07 12:33:44 by thugo             #+#    #+#              #
-#    Updated: 2016/11/10 19:26:47 by thugo            ###   ########.fr        #
+#    Updated: 2016/11/10 21:24:48 by thugo            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,7 +18,7 @@ FILE_SRC = ft_memset ft_bzero ft_memcpy ft_memccpy ft_memmove ft_memchr ft_memcm
 		   ft_memalloc ft_memdel ft_strnew ft_strdel ft_strclr ft_striter ft_striteri ft_strmap\
 		   ft_strmapi ft_strequ ft_strnequ ft_strsub ft_strjoin ft_strtrim ft_strsplit ft_abs\
 		   ft_itoa ft_putchar ft_putstr ft_putendl ft_putnbr ft_putchar_fd ft_putstr_fd\
-		   ft_putendl_fd ft_putnbr_fd
+		   ft_putendl_fd ft_putnbr_fd ft_lstnew
 
 .PHONY: all
 
---
+++ ft_lstnew.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/10 20:46:14 by thugo             #+#    #+#             */
+/*   Updated: 2016/11/10 21:25:21 by thugo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list		*list;
+	void		*content_cpy;
+
+	list = (t_list *)malloc(sizeof(t_list));
+	if (list == NULL)
+		return (NULL);
+	if (content == NULL)
+	{
+		list->content = NULL;
+		list->content_size = 0;
+	}
+	else
+	{
+		content_cpy = malloc(content_size);
+		if (content_cpy == NULL)
+		{
+			free(list);
+			return (NULL);
+		}
+		list->content = ft_memcpy(content_cpy, content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}

8f2b0d57bd64f4c4a61f7df0d690809722880f32
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:37:36 +0100

    Add include

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strjoin(char const *s1, char const *s2)

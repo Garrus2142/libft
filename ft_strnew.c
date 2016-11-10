11766e267e57a3cce6882a31bc0be87e71ad33f6
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:39:16 +0100

    Add include

--- ft_strnew.c
+++ ft_strnew.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 char	*ft_strnew(size_t size)

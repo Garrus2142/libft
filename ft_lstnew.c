96e979ff7d2e2108c63eb0a1d5d4d98d5d513b45
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:31:12 +0100

    Add include

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)

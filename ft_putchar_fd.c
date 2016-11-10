453b9bf85036b54127762748be9c68d8e281b520
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:34:59 +0100

    Add include

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "libft.h"
 
 void	ft_putchar_fd(char c, int fd)

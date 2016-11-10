a6f03b8682cef437e6e397faf10ecb6d891469c1
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:31:56 +0100

    Add include

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 void	*ft_memalloc(size_t size)

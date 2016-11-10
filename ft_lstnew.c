07459aa9f3c281d0b5519cc31d2c77559618b36e
Garrus2142 <Garrus2142@users.noreply.github.com>
2016-11-10 22:32:25 +0100

    Change stdio -> stdlib

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include <stdlib.h>
 #include "libft.h"
 
 t_list	*ft_lstnew(void const *content, size_t content_size)

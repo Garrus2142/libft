27d05e80e17e4bba7b98fdf3f87cbd8f201caaba
BuildTools <unconfigured@null.spigotmc.org>
2016-11-22 18:55:00 +0100

    Add ft_strndup

--- Makefile
+++ Makefile
@@ -19,7 +19,7 @@ FILE_SRC = ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memch
 		   ft_strmapi.c ft_strequ.c ft_strnequ.c ft_strsub.c ft_strjoin.c ft_strtrim.c ft_strsplit.c ft_abs.c\
 		   ft_itoa.c ft_putchar.c ft_putstr.c ft_putendl.c ft_putnbr.c ft_putchar_fd.c ft_putstr_fd.c\
 		   ft_putendl_fd.c ft_putnbr_fd.c ft_lstnew.c ft_lstdelone.c ft_lstdel.c ft_lstadd.c ft_lstiter.c\
-		   ft_memdup.c ft_lstmap.c ft_lstinsert.c ft_is_negative.c
+		   ft_memdup.c ft_lstmap.c ft_lstinsert.c ft_is_negative.c ft_strndup.c
 
 OBJ = $(FILE_SRC:.c=.o)
 
---
+++ ft_strndup.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strndup.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: thugo <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/22 18:20:45 by thugo             #+#    #+#             */
+/*   Updated: 2016/11/22 18:21:57 by thugo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_strndup(const char *s, size_t len)
+{
+	char	*new;
+	int		i;
+	size_t	size;
+
+	size = (int)ft_strlen(s);
+	new = (char *)malloc(sizeof(char) * (size > len ? len : size) + 1);
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0' && i < (int)len)
+	{
+		new[i] = s[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+
+}
